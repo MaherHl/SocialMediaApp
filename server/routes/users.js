e82e05e43caa72cdba6f201f2212e2f03285ef3b
MaherHl <97396412+MaherHl@users.noreply.github.com>
2023-08-11 09:38:46 +0100

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ import {addRemoveFriend,getUserFriends , getUSer} from "../controllers/users.js"
 import { verifyToken } from "../middleware/auth.js"
 const router = express.Router()
 router.get("/:id",verifyToken,getUSer)
-router.get("/:id/friens",verifyToken,getUserFriends)
+router.get("/:id/friends",verifyToken,getUserFriends)
 router.patch("/:id/:friendId",verifyToken,addRemoveFriend)
 
 
